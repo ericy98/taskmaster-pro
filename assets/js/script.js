ecece38e453274de7cc475ca3fef890e83ec1f9b
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-06-18 16:02:30 -0500

    add datepicker function

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,12 +107,20 @@ $(".list-group").on("click", "span", function() {
   // swap out elements 
   $(this).replaceWith(dateInput);
 
+  // enable jQuery UI picker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the `dateInput`
+      $(this).trigger("change");
+    }
+  });
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   
   // get current text
   var date = $(this)
@@ -142,6 +150,11 @@ $(".list-group").on("blur", "input[type='text']", function() {
   // replace input with <span> element 
   $(this).replaceWith(taskSpan);
 
+});
+
+// calendar widget to pick date
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // sortable jQuery UI
